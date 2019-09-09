8657e9f667fb09321bc69596d33c8020d5c9267d
Philipp Heuer <git@philippheuer.me>
2019-09-09 17:24:02 +0200

    feature: support windows / mac / linux 32&64 when installing / don't use aliases in scripts

--- install.sh
+++ install.sh
@@ -9,51 +9,63 @@ INSTALL_MODE=${INSTALL_MODE:-system} # system or project (prject creates a .ci/b
 CONFIG_DIR=${CONFIG_DIR:-/etc/envcli}
 TARGET_DIR=${TARGET_DIR:-/usr/local/bin}
 
+# detect os and arch
+case "$(uname -s)" in
+    Linux*)     OS=linux;;
+    Darwin*)    OS=darwin;;
+    CYGWIN*)    OS=windows;;
+    MINGW*)     OS=windows;;
+    *)          OS="UNKNOWN:$(uname -s)"
+esac
+case "$(uname -m)" in
+    x86_64*)    ARCH=amd64;;
+    i386*)      ARCH=386;;
+    *)          ARCH="UNKNOWN:$(uname -m)"
+esac
+
 # support for user-mode setup
 if echo "$INSTALL_MODE" | grep -q 'project'; then
-    TARGET_DIR=$(pwd)/.ci/bin
-    mkdir -p $(pwd)/.ci/bin
-    export PATH=$PATH:$(pwd)/.ci/bin
+    TARGET_DIR="$(pwd)/.ci/bin"
+    mkdir -p "$(pwd)/.ci/bin"
+    PATH=$PATH:$(pwd)/.ci/bin
+    export PATH
 fi
 
 # preq: envcli
-echo "-> Getting EnvCLI ..."
-if ! test -f $TARGET_DIR/envcli; then
-    curl -L -s -o $TARGET_DIR/envcli https://dl.bintray.com/envcli/golang/envcli/v0.6.0/envcli_linux_amd64
-    chmod +x $TARGET_DIR/envcli
+echo "-> getting envcli ..."
+if ! test -f "$TARGET_DIR/envcli"; then
+    echo "--> installing envcli into $TARGET_DIR"
+    curl -L -s -o "$TARGET_DIR/envcli" "https://dl.bintray.com/envcli/golang/envcli/v0.6.0/envcli_${OS}_${ARCH}"
+    chmod +x "$TARGET_DIR/envcli"
 fi
-echo "-> EnvCLI Configuration"
-mkdir -p $CONFIG_DIR
-envcli config set global-configuration-path $CONFIG_DIR
-chmod 644 $TARGET_DIR/.envclirc
+echo "-> configuring envcli"
+mkdir -p "$CONFIG_DIR"
+envcli config set global-configuration-path "$CONFIG_DIR"
+chmod 644 "$TARGET_DIR/.envclirc"
 if echo "$INSTALL_FROM" | grep -q 'remote'; then
-    curl -L -s -o $CONFIG_DIR/.envcli.yml https://raw.githubusercontent.com/EnvCLI/modular-pipeline/master/.envcli.yml
+    curl -L -s -o "$CONFIG_DIR/.envcli.yml" "https://raw.githubusercontent.com/EnvCLI/modular-pipeline/master/.envcli.yml"
 elif echo "$INSTALL_FROM" | grep -q 'local'; then
-    cp $LOCAL_PATH/.envcli.yml $CONFIG_DIR/.envcli.yml
+    cp "$LOCAL_PATH/.envcli.yml" "$CONFIG_DIR/.envcli.yml"
 fi
-chmod 644 $CONFIG_DIR/.envcli.yml
+chmod 644 "$CONFIG_DIR/.envcli.yml"
 
 # req: normalizeci
-echo "-> Getting NormalizeCI ..."
-if ! test -f $TARGET_DIR/normalizeci; then
-    curl -L -s -o $TARGET_DIR/normalizeci https://dl.bintray.com/envcli/golang/normalize-ci/v0.0.1/linux_amd64
-    chmod +x $TARGET_DIR/normalizeci
+echo "-> getting normalize-ci ..."
+if ! test -f "$TARGET_DIR/normalizeci"; then
+    curl -L -s -o "$TARGET_DIR/normalizeci" "https://dl.bintray.com/envcli/golang/normalize-ci/v0.0.1/${OS}_${ARCH}"
+    chmod +x "$TARGET_DIR/normalizeci"
 fi
 
 # pipeline
-echo "-> Getting CI Scripts ..."
-ACTION_LIST=("action-common" "ci-debug" "action-go-test" "action-go-build" "action-java-build" "action-optimize-upx" "action-package-dockerfile")
+echo "-> getting pipeline scripts ..."
+# - actions
+ACTION_LIST=("action-common" "action-ci-debug" "action-common-go" "action-go-test" "action-go-build" "action-common-java" "action-java-test" "action-java-build" "action-optimize-upx" "action-package-dockerfile" "action-common-deploy" "action-swarm-deploy")
 for i in "${ACTION_LIST[@]}"; do
-    echo "--> Action: $i"
+    echo "--> action: $i"
     if echo "$INSTALL_FROM" | grep -q 'remote'; then
-        curl -L -s -o $TARGET_DIR/$i https://raw.githubusercontent.com/EnvCLI/modular-pipeline/master/actions/$i
-        chmod +x $TARGET_DIR/$i
+        curl -L -s -o "$TARGET_DIR/$i" "https://raw.githubusercontent.com/EnvCLI/modular-pipeline/master/actions/$i"
     elif echo "$INSTALL_FROM" | grep -q 'local'; then
-        cp $LOCAL_PATH/actions/$i $TARGET_DIR/$i
-        chmod +x $TARGET_DIR/$i
+        cp "$LOCAL_PATH/actions/$i" "$TARGET_DIR/$i"
     fi
+    chmod +x "$TARGET_DIR/$i"
 done
-
-# aliases
-echo "-> Installing EnvCLI Aliases"
-envcli install-aliases
