fe7e3dd823b1b84fd6c6c82860dd9e34e0fe3701
Philipp Heuer <git@philippheuer.me>
2019-11-16 16:37:25 +0100

    fix: support golang in prepare stage

--- src/libexec/mpi/stages/stage-prepare.bash
+++ src/libexec/mpi/stages/stage-prepare.bash
@@ -10,33 +10,23 @@ set -euo pipefail
 function main()
 {
   # container
-  if [[ ${PROJECT_TYPE} =~ ^container-.*$ ]]; then
+  if [[ ${PROJECT_TYPE} =~ ^container$ ]]; then
     @mpi.log_message "INFO" "does not need any preparation."
-    exit 0
+    return 0
   fi
 
-  # container
-  if [[ ${PROJECT_TYPE} =~ ^golang-.*$ ]]; then
+  # golang
+  if [[ ${PROJECT_TYPE} =~ ^golang*$ ]]; then
     @mpi.log_message "INFO" "pulling images ..."
     envcli pull-image go upx
-    exit 0
-  fi
-
-  # deployment
-  if [[ ${PROJECT_TYPE} =~ ^deployment*$ ]]; then
-    # check for invalid combinations
-    if [[ ${DEPLOYMENT_TYPE} =~ ^none*$ ]]; then
-      @mpi.log_message "ERROR" "project type [${PROJECT_TYPE}] is not supported with deployment type [${DEPLOYMENT_TYPE}]!"
-      exit 1
-    fi
-    exit 0
+    return 0
   fi
 
   # java
-  if [[ ${PROJECT_TYPE} =~ ^java-.*$ ]]; then
+  if [[ ${PROJECT_TYPE} =~ ^java$ ]]; then
     @mpi.log_message "INFO" "pulling images ..."
     envcli pull-image java
-    exit 0
+    return 0
   fi
 
   # no match
