32175c2d3f267327c94a85334eaad57750715705
Philipp Heuer <git@philippheuer.me>
2020-05-24 16:29:01 +0200

    feature: allow to specify a custom discord changelog template

--- src/libexec/mpi/stages/stage-documentation.bash
+++ src/libexec/mpi/stages/stage-documentation.bash
@@ -26,7 +26,7 @@ function main()
     # notification
     # - discord
     if [ -n "$RELEASE_WEBHOOK_DISCORD" ]; then
-      CHANGELOG_CONFIG_FILE="$TMP_DIR/changelog/publish-release-markdown-discord.yml" @mpi action changelog-generate --ref="$NCI_COMMIT_REF_NAME" --output="${TMP_DIR}/changelog-discord"
+      CHANGELOG_CONFIG_FILE="${CHANGELOG_CONFIG_FILE_DISCORD:-$TMP_DIR/changelog/publish-release-markdown-discord.yml}" @mpi action changelog-generate --ref="$NCI_COMMIT_REF_NAME" --output="${TMP_DIR}/changelog-discord"
       @mpi.discord.send_webhook "RELEASE_WEBHOOK_DISCORD" "Releasebot" "$(<${TMP_DIR}/changelog-discord)"
     fi
     # - rocketchat
