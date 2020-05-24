cc288758d33e3cbb411076a2a5a7e4d4897596cb
Philipp Heuer <git@philippheuer.me>
2020-05-24 19:31:54 +0200

    fix: use the correct method names to send messages to rocketchat / discord

--- src/libexec/mpi/stages/stage-documentation.bash
+++ src/libexec/mpi/stages/stage-documentation.bash
@@ -27,11 +27,11 @@ function main()
     # - discord
     if [ -n "$RELEASE_WEBHOOK_DISCORD" ]; then
       CHANGELOG_CONFIG_FILE="${CHANGELOG_CONFIG_FILE_DISCORD:-$TMP_DIR/changelog/publish-release-markdown-discord.yml}" @mpi action changelog-generate --ref="$NCI_COMMIT_REF_NAME" --output="${TMP_DIR}/changelog-discord"
-      @mpi.discord.send_webhook "RELEASE_WEBHOOK_DISCORD" "Releasebot" "$(<${TMP_DIR}/changelog-discord)"
+      @mpi.discord.send_message "RELEASE_WEBHOOK_DISCORD" "Releasebot" "$(<${TMP_DIR}/changelog-discord)"
     fi
     # - rocketchat
     if [ -n "$RELEASE_WEBHOOK_ROCKETCHAT" ]; then
-      @mpi.rocketchat.send_webhook "RELEASE_WEBHOOK_ROCKETCHAT" "Releasebot" "$(<${TMP_DIR}/changelog.md)"
+      @mpi.rocketchat.send_message "RELEASE_WEBHOOK_ROCKETCHAT" "Releasebot" "$(<${TMP_DIR}/changelog.md)"
     fi
   fi
 }
